0863a23db309f39d3344d12f19516a7c9dfe2809
Klein <klein.gachuhi@student.moringaschool.com>
2025-05-18 12:13:49 +0300

    Something about contracts between booksand authors

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,65 @@
 class Author:
-    pass
+    all = []
+
+    def __init__(self, name):
+        self.name = name
+        Author.all.append(self)
+
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.author == self]
+    
+    def books(self):
+        return [contract.book for contract in self.contracts()]
+    
+    def sign_contract(self, book: 'Book', date: str, royalties: int):
+        return Contract(self, book, date, royalties)
+    
+    def total_royalties(self):
+        return sum([contract.royalties for contract in self.contracts()])
+
 
 
 class Book:
-    pass
+    all = []
+    def __init__(self, title: str):
+        self.title = title
+        Book.all.append(self)
+
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.book == self]
+    
+    def authors(self):
+        return [contract.author for contract in self.contracts()]
 
 
 class Contract:
-    pass
+    all = []
+    def __init__(self, author, book, date, royalties):
+        if isinstance(author, Author):
+            self.author = author
+        else:
+            raise Exception("Author must be an instance of the author class")
+        if isinstance(book, Book):
+            self.book = book
+        else:
+            raise Exception("Book must be an instance of the book class")
+        if isinstance(date, str):
+            self.date = date
+        else:
+            raise Exception("Date must be a string")
+        if isinstance(royalties, int):
+            self.royalties = royalties
+        else:
+            raise Exception("Royalty must  be an instance  of the Contract class")
+        Contract.all.append(self)
+        
+
+    @classmethod
+    def contracts_by_date(cls, date):
+        return [contract for contract in cls.all if contract.date == date]
+
+book = Book("Title")
+author = Author("Name")
+date = '01/01/2001'
+royalties = 40000
+contract = Contract(author, book, date, royalties)
